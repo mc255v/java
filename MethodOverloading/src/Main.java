3c5d318af67a980e4040cc84de6e0aa730a095a1
Matthew Cook <mc255v@gmail.com>
2019-12-29 12:14:59 +0900

    refactored dir structure

---
+++ MethodOverloading/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 100;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unknown player scored " + score + " points");
+        return score * 1000;
+    }
+}
