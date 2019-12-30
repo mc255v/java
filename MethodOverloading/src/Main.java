15b6f3304de43b1d7e280ff4f46c5d5870563206
Matthew Cook <mc255v@gmail.com>
2019-12-30 11:33:47 +0900

    testing and configuring intellij

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         int newScore = calculateScore("Tim", 500);
         System.out.println("New score is " + newScore);
         calculateScore(75);
+        calculateScore();
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -14,5 +15,10 @@ public class Main {
     public static int calculateScore(int score) {
         System.out.println("Unknown player scored " + score + " points");
         return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name or score here");
+        return 0;
     }
 }
